54d97c9c0b03a68e42dc95407e404ff0949b4592
MasterFenrir <masterfenrir@outlook.com>
2016-09-21 16:43:13 +0200

    ADD: Generator finding capabalities, but needs refactoring

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,9 +2,11 @@ import org.apache.commons.cli.*;
 import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by Sander on 21-9-2016.
@@ -33,7 +35,7 @@ public class Controller {
     }
 
     private void printHelpPage() {
-        System.out.println("Usage: <number> <power> <modulo>");
+        System.out.println("Usage: <number>");
     }
 
     public void run() throws ParseException {
@@ -42,7 +44,56 @@ public class Controller {
             return;
         }
 
+        String[] arguments = commandLine.getArgs();
+
+        if (arguments.length != 1 || !StringUtils.isNumeric(arguments[0])) {
+            printHelpPage();
+            throw new ParseException("Invalid arguments, one and only one prime number is required");
+        }
+
+        int number = Integer.parseInt(arguments[0]);
+
+        List<Integer> factors = primeFactorization(number - 1);
+        System.out.println(factors);
+        List<Integer> validGenerators = IntStream.range(2, number)
+                .filter(generator -> validGenerator(generator, number, factors))
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(validGenerators);
     }
 
+    private boolean validGenerator(int generator, int number, List<Integer> factors) {
+        List<Integer> invalidGenerators = factors.stream()
+                .filter(e -> generatorFormula(generator, number, e))
+                .collect(Collectors.toList());
+        return invalidGenerators.size() == 0;
+    }
+
+    private boolean generatorFormula(int generator, int number, int factor) {
+        BigInteger bigGenerator = BigInteger.valueOf(generator);
+        BigInteger bigNumber = BigInteger.valueOf(number);
+        BigInteger bigFactor = BigInteger.valueOf(factor);
+
+        BigInteger power = bigNumber.subtract(BigInteger.ONE).divide(bigFactor);
+        BigInteger genPowered = bigGenerator.modPow(power, bigNumber);
+
+        return genPowered.equals(BigInteger.ONE);
+    }
+
+    private List<Integer> primeFactorization(int number) {
+        List<Integer> factors = new ArrayList<>();
+        int temp = number;
+        for (int i = 2; i <= temp / i; i++) {
+            while (temp % i == 0) {
+                factors.add(i);
+                temp /= i;
+            }
+        }
+        if (temp > 1) {
+            factors.add(temp);
+        }
+
+        return factors;
+    }
 
 }
